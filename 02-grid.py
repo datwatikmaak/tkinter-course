315a84429d0fc3434f1ec094b6f41f588491b268
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-07 10:12:15 +0200

    final code grid.py

--- 02-grid.py
+++ 02-grid.py
@@ -3,8 +3,11 @@ import tkinter as tk
 root = tk.Tk()
 
 # creating a label widget
-myLabel = tk.Label(root, text="Hello World!")
-# shoving it onto the screen
-myLabel.pack()
+myLabel1 = tk.Label(root, text="Hello World!")
+myLabel2 = tk.Label(root, text="My name is Leonieke")
+
+# placing the labels with the .grid()
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=1)
 
 root.mainloop()
