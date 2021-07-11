36ccef4db25fbc5b56db3b3f3784712304eb0edf
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-11 12:29:17 +0200

    final code buttons.py

--- 03-buttons.py
+++ 03-buttons.py
@@ -2,12 +2,13 @@ import tkinter as tk
 
 root = tk.Tk()
 
-# creating a label widget
-myLabel1 = tk.Label(root, text="Hello World!")
-myLabel2 = tk.Label(root, text="My name is Leonieke")
 
-# placing the labels with the .grid()
-myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=1)
+def my_click():
+    my_label = tk.Label(root, text="Look! I clicked a button!")
+    my_label.pack()
+
+
+my_button = tk.Button(root, text="Click Me!", padx=50, pady=10, command=my_click, fg="blue", bg="green")
+my_button.pack()
 
 root.mainloop()
