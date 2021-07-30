d80894feb36798baea383db3636c14d418d4aae0
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-30 13:26:52 +0200

    Working with radio buttons. Final code.

--- 10_radio_buttons.py
+++ 10_radio_buttons.py
@@ -5,7 +5,25 @@ root = Tk()
 root.title("Radio Buttons")
 root.iconbitmap("img/python.ico")
 
+MODES = [
+    ("Pepperoni", "Pepperoni"),
+    ("Cheese", "Cheese"),
+    ("Mushroom", "Mushroom"),
+    ("Onion", "Onion")
+]
 
 
+def clicked(value):
+    my_label = Label(root, text=value).pack()
+    my_label.pack()
+
+
+pizza = StringVar()
+pizza.set("Pepperoni")
+
+for text, mode in MODES:
+    Radiobutton(root, text=text, variable=pizza, value=mode).pack(anchor=W)
+
+my_button = Button(root, text="Click Me!", command=lambda: clicked(pizza.get())).pack()
 
 mainloop()
